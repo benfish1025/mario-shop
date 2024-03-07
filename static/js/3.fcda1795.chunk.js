111f6c856e186b7b64dba9fd15a9ff73e23b5c3c
benfish1025 <benfish1025@users.noreply.github.com>
2024-03-07 02:02:28 +0000

    deploy: 30413d523ee1cbd4e52f594346f7044b18854144

---
+++ static/js/3.fcda1795.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmario-shop"]=this["webpackJsonpmario-shop"]||[]).push([[3],{45:function(o,s,e){"use strict";o.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=3.fcda1795.chunk.js.map
